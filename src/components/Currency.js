7c1963bbb54033f9246332d78b709e72351c06c3
OrnelieManz <ornelie.manz@gmail.com>
2024-06-26 15:57:18 +0200

    updating Currency function codes

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -26,11 +26,11 @@ const Currency = (props) => {
         <div className="dropdown">
         <button className="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" style={{background:"lightgreen",border:"transparent"}} 
         aria-haspopup="true" aria-expanded="true" >{selectedItem}<span className="caret"></span></button>
-        <ul className="dropdown-menu show"  style={{background:"lightgreen"}}>
-            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} id="Dollar" value="$" name="$ Dollar" onClick={handleselectItem}>$ Dollar</button></li>
-            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="£" name="£ Pound" onClick={handleselectItem}>£ Pound</button></li>
-            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="€" name="€ Euro" onClick={handleselectItem}>€ Euro </button></li>
-            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="₹" name="₹ Rupee" onClick={handleselectItem}>₹ Rupee</button></li>
+        <ul className="dropdown-menu"  style={{background:"lightgreen"}}>
+            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} id="Dollar" value="$" name="Currency ($ Dollar)" onClick={handleselectItem}>$ Dollar</button></li>
+            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="£" name="Currency (£ Pound)" onClick={handleselectItem}>£ Pound</button></li>
+            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="€" name="Currency (€ Euro)" onClick={handleselectItem}>€ Euro </button></li>
+            <li ><button className="CurrencyChoice" style={{background:"none", width:"160px",paddingLeft:"15px",textAlign:"left","hover":{backgroundColor:"white"},border:"transparent"}} value="₹" name="Currency (₹ Rupee)" onClick={handleselectItem}>₹ Rupee</button></li>
             </ul>
     </div>
 
